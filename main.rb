b4f0c96b122d9e1c27fdf28b7ae144b64799e1a1
Rafael César Neves <rafael.neves@humantech.com.br>
2016-04-15 15:37:20 -0300

    Trying with Threads

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@
 
 require 'nokogiri'
 require 'open-uri'
-# require 'fileutils'
 
 class Bible
   @base_url = "http://www.biblica.com"
@@ -12,31 +11,58 @@ class Bible
   def self.create_download_files
     main_body = Nokogiri::HTML(open(@base_url + @first_href))
     books = main_body.css(".bible-nav .large ul.dropdown-menu li").map(&:children).flatten.map{|i| i["href"]}
+    process = []
 
-    [0..9, 10..19, 20..29, 30..39, 40..49, 50..59, 60..65].each do |interval|
-      books[interval].each do |book|
-        fork do
-          book_body = Nokogiri::HTML(open(@base_url + book))
-          chapters = book_body.css(".bible-nav .small ul.dropdown-menu li").map(&:children).flatten.map{|i| i["href"]}
-          folder = book_body.css(".bible-nav .large .btn.dropdown-toggle").first.text
-          dirs = []
-
-          chapters.each do |chapter|
-            chapter_body = Nokogiri::HTML(open(@base_url + chapter))
-            download_elem = chapter_body.css("audio > source[type='audio/mpeg']").first
-
-            dirs << download_elem["src"]
-            print "#"
-          end
-
-          filename = File.join(@urls_path, folder)
-          File.open(filename, "w") do |file|
-            file.puts dirs
+    progress = Thread.new do
+      while true
+        system "clear"
+        Thread.current.thread_variable_get(:process).each do |p|
+          begin
+            puts p.thread_variable_get(:folder) + " " + p.thread_variable_get(:progress)
+          rescue
           end
         end
+        sleep 1
+        break if Thread.current.thread_variable_get(:finish)
       end
-      Process.waitall
     end
+
+    books.each do |book|
+      process << Thread.new do
+        book_body = Nokogiri::HTML(open(@base_url + book))
+        chapters = book_body.css(".bible-nav .small ul.dropdown-menu li").map(&:children).flatten.map{|i| i["href"]}
+        folder = book_body.css(".bible-nav .large .btn.dropdown-toggle").first.text
+        Thread.current.thread_variable_set(:folder, folder)
+        dirs = []
+
+        chapters.each do |chapter|
+          chapter_body = Nokogiri::HTML(open(@base_url + chapter))
+          download_elem = chapter_body.css("audio > source[type='audio/mpeg']").first
+
+          dirs << download_elem["src"]
+          Thread.current.thread_variable_set(:progress, "#{chapter.split('/').last}/#{chapters.size}")
+        end
+
+        filename = File.join(@urls_path, folder)
+        File.open(filename, "w") do |file|
+          file.puts dirs
+        end
+      end
+
+      progress.thread_variable_set(:process, process)
+
+      while process.size >= 10 do
+        process.each do |p|
+          unless p.status
+            process.delete(p)
+          end
+        end
+        sleep 1
+      end
+    end
+
+    process.each(&:join)
+    progress.thread_variable_set(:finish, true)
   end
 
   def self.download_audios
@@ -45,6 +71,7 @@ class Bible
       `wget -q --directory-prefix='downloads/#{file.split('/').last}' -i #{file}`
     end
   end
+
 end
 
 Bible.create_download_files
